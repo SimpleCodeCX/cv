3633db737c86f492f18e131c09d011ffa2ba0be0
SimpleCodeCX <248200851@qq.com>
2019-03-31 19:37:12 +0800

    feat(skill): use @dataSource & add console

--- src/app/pages/skills/skills.ts
+++ src/app/pages/skills/skills.ts
@@ -8,3 +8,14 @@ const skillsArt = require('./skills.art');
 const skillsHtml = skillsArt({ skillList: skillValueList });
 document.getElementById('skills').innerHTML = skillsHtml;
 
+setTimeout(async () => {
+
+  const skillService = await import('./skill.service');
+  const consoleHelper = await import('./console.helper');
+
+  const skillListServer = await skillService.getAllSkill();
+  // 控制台打印我的 skill list
+  consoleHelper.consoleSkills(skillListServer);
+
+}, 1000);
+
