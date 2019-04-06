f11b31aa74e6f30293c07c5f32ae6128740b207d
SimpleCodeCX <248200851@qq.com>
2019-04-06 12:12:42 +0800

    refactor(@dataSource): update api host

--- src/app/@dataSource/api/url.ts
+++ src/app/@dataSource/api/url.ts
@@ -1,10 +1,9 @@
 /**********************************************************************/
 /* public local url -> http://localhost:8080                      */
 /**********************************************************************/
-export const HOST_URL = 'http://localhost:8080';
+// export const HOST_URL = 'http://localhost:8080';
 
 /**********************************************************************/
-/* public service url -> http://148.70.160.165:8080                      */
+/* public service url -> http://www.cxdsimple.com/cv-server                     */
 /**********************************************************************/
-// export const HOST_URL = 'http://148.70.160.165:8080';
-
+export const HOST_URL = 'http://www.cxdsimple.com/cv-server';
